c8d81f6d57a7f5f8931e5882a0f0bb411fd33d94
Sachin Bhagat <sachinbhagat0307@gmail.com>
2019-07-18 16:02:36 +0530

    added webdriver exception

---
+++ src/SeleniumCode/WebDriver_Exception.java
@@ -0,0 +1,10 @@
+package SeleniumCode;
+
+public class WebDriver_Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SeleniumCode/WebDriver_Hierarchy.java
@@ -0,0 +1,25 @@
+package SeleniumCode;
+
+// Q. Why WebDriver driver = new ChromeDriver(); ?
+
+// WebDriver Hierarchy
+//=========================
+//
+//                         SearchContext(I)
+//                               | extends
+//                           WebDriver(I)
+//                               | implements
+// JavaScriptExecutor(I)--> RemoteWebDriver(C)<-- TakeScreenshot(I)
+//                               | extends
+//                          ChromeDriver(C)
+//                          FirefoxDriver(C)
+
+
+public class WebDriver_Hierarchy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
