66c2659d601517b87dd00c8a5699a280a267627e
Sachin Bhagat <sachinbhagat0307@gmail.com>
2019-07-28 17:21:30 +0530

    added readExcel

--- src/testNG/TestNG_DataProviders.java
+++ src/testNG/TestNG_DataProviders.java
@@ -6,37 +6,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-// Q . How to pass parameter to TestNG?
+// Q . What is the use of DataProvider?
 
+// -- DataProvider is use to test with multiple sets of data.
 /*
-<suite name ="testNgParameterTest">
+XML=============
 
-    <test name="parameterTest" preserve-order = "true">
+<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">
+<suite name ="testNgDataProvidersTest">
+	<test name="dataProviders" preserve-order = "true">
 	<parameter name ="browser" value ="chrome" />
-	<parameter name ="name" value ="Sachin Bhagat" />
-	<parameter name ="email" value ="sachinbht37@gmail.com" />
-	<parameter name ="website" value ="https://www.mywebsite.com" />
-	<parameter name ="comments" value ="Hi sachin..! This is my Comments" />
 		<classes>
-			<class name="testNG.TestNG_Parameter"></class>
+			<class name="testNG.TestNG_DataProviders">
+				<methods>
+					<include name ="registerForm" />
+				</methods>
+			</class>
 		</classes>
-    </test>
-		
+	</test>
 </suite>
 
 */
-public class TestNG_Parameter {
+public class TestNG_DataProviders {
 	
 	WebDriver driver;
 	String baseUrl;
 	
 	@Parameters("browser")
-	@BeforeClass
+	@BeforeMethod
 	public void setUp(String browser) {
 		baseUrl = "http://www.globalsqa.com/samplepagetest/";
 		
@@ -51,8 +54,14 @@ public class TestNG_Parameter {
 		driver.get(baseUrl);
 	}
 	
-	@Parameters({"name", "email", "website", "comments"})
-	@Test
+	
+	@DataProvider(name="fieldInputs")
+	public static Object[][] fillData(){
+		return new Object[][] {{"sachin bhagat","sachinbhagat0307@gmail.com", "https://www.website1.com", "Comments one"},
+				               {"ajay bhagat", "ajaybhagat@gmail.com", "https://www.website2.com", "comments2" }};
+	}
+	
+	@Test(dataProvider="fieldInputs")
 	public void registerForm(String name, String email, String website, String comments) {
 		
 		driver.findElement(By.id("g2599-name")).sendKeys(name);
